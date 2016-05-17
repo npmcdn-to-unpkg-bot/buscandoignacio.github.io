e206d58b8f46ab3b89c903b1c3d36c5f358654c4
buscandoignacio <buscandoignacio@gmail.com>
2016-05-16 23:49:58 -0300

    Fixed Map Animation
    Fixed Map Animation

--- prod/js/controllers/main.js
+++ prod/js/controllers/main.js
@@ -8,8 +8,9 @@
     hiddeLoader = function(){
         var $loader = $('.preLoader');
         $loader.fadeOut('slow', function(){
+            $('body').toggleClass('preloader-ison');
             $('.preLoader-on').each(function(index){
-                $(this).delay(800*index).fadeIn('slow');    
+                $(this).delay(800*index).fadeTo('slow', 1);    
             });  
         });  
     };
